af2a97443f00484b7110d4486fffcc499fd54974
Mona Lisa <holmesmainsend@gmail.com>
2021-12-16 12:39:27 -0500

    successfully got first route, api/categories

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      validate: {
+        isDecimal: true
+      },
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: '10',
+      validate: {
+        isNumeric: true
+      },
+      allowNull: false,
+    }
   },
   {
     sequelize,
